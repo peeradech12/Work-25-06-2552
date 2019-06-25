85f5b721438c0b39b3bff4b9ae6ce9542af0d78d
peeradech12 <51998280+peeradech12@users.noreply.github.com>
2019-06-25 17:18:13 +0700

    Add file .cpp

---
+++ Ex2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main ()
+{
+	string Name = "Peeradech Sadsum-ang";
+	int point1 = 85, point2 = 90;
+	char grade = 'A';
+	float GPA = 3.80;
+	cout << "My name is : " << Name << endl;
+	cout << "My score1 : " << point1 <<" points" <<endl;
+	cout << "My score2 : " << point2 <<" points" <<endl;
+	cout << "Total : " << point1 + point2 <<" points" <<endl;
+	cout << "Grade : " << grade  <<endl;
+	cout << "GPA : " << GPA << endl;
+	return 0;
+
+
+
+
+}
